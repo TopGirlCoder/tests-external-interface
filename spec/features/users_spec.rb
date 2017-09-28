961f38e60c3c4b80a89bb4524b1dc4989df1ee3f
Tanya Lindermeier <topgirlcoder@gmail.com>
2017-09-28 00:29:33 -0700

    refactoring tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -4,105 +4,127 @@ require_relative '../user'
 RSpec.feature User, type: :feature do
 
   before(:all) { @user = User.new(first_name: 'test', last_name: 'test', phone_number: '4155551234', email: "#{Random.rand(1000)}@test#{Random.rand(1000)}.com", password: '12345678') } 
+  before(:all) { @user_invalid_email = User.new(first_name: 'test', last_name: 'test', phone_number: '4155551234', email: 'test', password: '12345678') } 
+  before(:all) { @user_invalid_password = User.new(first_name: 'test', last_name: 'test', phone_number: '4155551234', email: "#{Random.rand(1000)}@test#{Random.rand(1000)}.com", password: '123') } 
 
   feature 'visits the root page' do
     before { visit '/' }
 
-    scenario 'should be true if has url' do
+    scenario 'with url' do
       expect(URI.parse(current_url).to_s).to eq 'http://pai-test.herokuapp.com/'
       expect(URI.parse(current_url).to_s).to_not eq 'http://non-existent-site.com'
     end 
 
-    scenario 'should be true if has content' do
+    scenario 'with content' do
       expect(page).to have_content 'Publicity.ai'
       expect(page).not_to have_content 'non-existent-content'
     end      
 
-    scenario 'should be true if has sign up link' do
+    scenario 'with sign up link' do
       expect(page).to have_link 'Try It Free', href: '/users/sign_up'
       expect(page).not_to have_link 'Non existent link', href: '/users/sign_up'
     end
 
-    scenario 'should be true if has sign in link' do 
+    scenario 'with sign in link' do 
       expect(page).to have_link 'Log in', href: '/users/sign_in'
       expect(page).not_to have_link 'Non existent link', href: '/users/sign_in'
     end    
   end 
 
-  feature 'signs up' do
+  feature 'visits the sign up page' do
     before { visit '/users/sign_up' }
 
-    scenario 'should be true if has url' do
+    scenario 'with url' do
       expect(URI.parse(current_url).to_s).to eq('http://pai-test.herokuapp.com/users/sign_up')
       expect(URI.parse(current_url).to_s).to_not eq 'http://non-existent-site.com'
     end  
 
-    scenario 'should be true if has content' do
+    scenario 'with content' do
       expect(page).to have_content 'Try Publicity.ai free'
       expect(page).not_to have_content 'non-existent-content'
     end      
 
-    scenario 'should be true if has button' do
+    scenario 'with button' do
       expect(page).to have_button 'Start Analyzing »'
       expect(page).not_to have_button 'non-existent-button'      
     end
 
-    scenario 'should be true if has form' do
+    scenario 'with form' do
       expect(page).to have_css 'form#new_user'
       expect(page).not_to have_css 'form.non-existent-form'
     end    
 
-    scenario 'should be true if signs up' do
+    scenario 'with valid email and password' do
       @user.sign_up
       expect(page).to have_content 'Welcome! You have signed up successfully.'
       expect(page).to have_link 'Logout', href: '/users/sign_out'
       expect(page).not_to have_link 'Try It Free', href: '/users/sign_up'
       expect(page).not_to have_css 'div.flash.error' 
-    end         
+    end  
+
+    scenario 'with invalid email' do
+      @user_invalid_email.sign_up
+      expect(page).to have_content 'Please enter a valid email address.'
+    end     
+
+    scenario 'with invalid password' do
+      @user_invalid_password.sign_up
+      expect(page).to have_css 'div.alert-danger'
+    end           
   end
 
-  feature 'signs in' do
+  feature 'visits the sign in page' do
     before { visit '/users/sign_in' }
 
-    scenario 'should be true if has url' do
+    scenario 'with url' do
       expect(URI.parse(current_url).to_s).to eq 'http://pai-test.herokuapp.com/users/sign_in'
       expect(URI.parse(current_url).to_s).to_not eq('http://non-existent-site.com')
     end   
 
-    scenario 'should be true if has content' do
+    scenario 'with content' do
       expect(page).to have_content 'Log in'
       expect(page).not_to have_content 'non-existent-content'
     end     
    
-    scenario 'should be true if has button' do
+    scenario 'with button' do
       expect(page).to have_button 'Sign in'
       expect(page).not_to have_button 'non-existent-button'
     end
 
-    scenario 'should be true if has form' do
+    scenario 'with form' do
       expect(page).to have_css 'form#new_user'
       expect(page).not_to have_css 'form.non-existent-form'
     end    
      
-    scenario 'should be true if signs in' do
+    scenario 'with valid email and password' do
       @user.sign_in
       expect(page).to have_content 'Signed in successfully.'
       expect(page).to have_link @user.first_name, href: '/users/edit'
       expect(page).not_to have_content 'Not signed in successfully.'
       expect(page).not_to have_button 'Sign in'
-      expect(page).not_to have_css 'div.flash.error'
+      expect(page).not_to have_css 'flash.error'
     end  
+
+    # scenario 'with invalid email' do
+    #   @user_invalid_email.sign_in
+    #   expect(page).to have_css 'flash.alert'
+    # end 
+
+    scenario 'with invalid password' do
+      @user_invalid_password.sign_in
+      expect(page).to have_css 'div.alert-danger'
+    end         
   end  
 
-  feature 'Signs out' do 
+  feature 'clicks to sign out' do 
     before { visit '/users/sign_in' }
 
-    scenario 'should be true if has link' do
+    scenario 'with link' do
       @user.sign_in
       expect(page).to have_link 'Logout', href: '/users/sign_out'            
     end 
 
-    scenario 'should be true if signs out' do
+    scenario 'and signs out' do
       @user.sign_out
       expect(page).to have_link 'Try It Free', href: '/users/sign_up'
       expect(page).to_not have_link 'Logout', href: '/users/sign_out'
